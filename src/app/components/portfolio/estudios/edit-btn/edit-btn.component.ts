d4a10fdb27edbb49f8331a6e6a7d86b5ec0338a7
Juan Carlos Torres <jucator368@gmail.com>
2023-04-12 00:39:50 -0300

    agregado un botón para restaurar los cambios en el boton editar

--- src/app/components/portfolio/estudios/edit-btn/edit-btn.component.ts
+++ src/app/components/portfolio/estudios/edit-btn/edit-btn.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Study } from 'src/app/model/study.model';
 import { StudyService } from 'src/app/services/study.service';
 
@@ -11,22 +19,50 @@ export class EditBtnComponent implements OnInit {
   @Input() study!: Study;
   @Output() onEditStudy: EventEmitter<Study> = new EventEmitter();
   @Output() closeEditBtn = new EventEmitter<boolean>();
+  @ViewChild('studyForm') studyForm!: NgForm;
+
+  originalStudy!: Study;
+  formChanged: boolean = false;
 
   constructor(private studyServ: StudyService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.originalStudy = { ...this.study };
+  }
+
+  cancel(form: NgForm): void {
+    this.study = { ...this.originalStudy };
+        form.resetForm(this.originalStudy);
+        this.formChanged = false;
+  }
 
   close(): void {
-    this.closeEditBtn.emit(true);
+    if (!this.formChanged) {
+      this.closeEditBtn.emit(true);
+    } else {
+      console.log('Form has unsaved changes. Are you sure you want to close?');
+    }
   }
 
   onSubmit(): void {
     if (this.study) {
-      this.studyServ.update(this.study.study_id, this.study).subscribe((data) => {
-        this.onEditStudy.emit(data);
-        this.closeEditBtn.emit(true);
-      });
+      this.studyServ
+        .update(this.study.study_id, this.study)
+        .subscribe((data) => {
+          this.onEditStudy.emit(data);
+          this.formChanged = false;
+          this.closeEditBtn.emit(true);
+        });
     }
   }
 
+  onFormChange(): void {
+    this.formChanged = true;
+  }
+
+  resetForm(form: NgForm): void {
+    this.study = { ...this.originalStudy };
+    form.resetForm(this.originalStudy);
+    this.formChanged = false;
+  }
 }
